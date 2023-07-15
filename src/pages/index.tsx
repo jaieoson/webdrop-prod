deedd1d12adb483bd9f50cdda27b03472ee7c6d4
jaieosom <jaieosom@gmail.com>
2023-07-14 23:04:22 -0300

    teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -87,7 +87,7 @@ function App(props: any) {
 <section className="overflow-hidden text-gray-700 ">
 <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
 <div className="flex flex-wrap -m-1 md:-m-2">
-   kkkkkkkkkkkkkkkkkkkkkkkk
+   OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
     </div>
   </div>
 </section>
